c4c67319bf8a45fef2613b88dbf5a2e67b372fa3
manjirinargunde <140157147+manjirinargunde@users.noreply.github.com>
2023-07-21 17:44:10 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,14 +2,14 @@ class PrimeNumbers
 {
    public static void main (String[] args)
    {		
-       int i =0;
+       i =0;
        int num =0;
        //Empty String
        String  primeNumbers = "";
 
        for (i = 1; i <= 100; i++)         
        { 		  	  
-          int counter=0; 	  
+          //int counter=0; 	  
           for(num =i; num>=1; num--)
 	  {
              if(i%num==0)
